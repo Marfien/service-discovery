5ed29675622e9ed7e3c87aa9f907ba01835df6eb
Marfien <marvin.sempa.haase@gmail.com>
2023-01-05 01:25:29 +0100

    Dockerize

--- registry/src/main/kotlin/dev/marfien/servicediscovery/registry/RegistryApplication.kt
+++ registry/src/main/kotlin/dev/marfien/servicediscovery/registry/RegistryApplication.kt
@@ -7,5 +7,11 @@ import org.springframework.boot.runApplication
 class RegistryApplication
 
 fun main(args: Array<String>) {
+    checkEnvironmentVariables()
     runApplication<RegistryApplication>(*args)
+}
+
+private fun checkEnvironmentVariables() {
+    if (System.getenv("MONGODB_URI") == null)
+        throw IllegalStateException("Cannot find 'MONGODB_URI' environment variable.")
 }
